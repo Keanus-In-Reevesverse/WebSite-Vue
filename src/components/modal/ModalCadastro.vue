623407998b12a7dac3b218be9802163851b2bf76
Bruno Cunha e Souza <brunosouza.cup@gmail.com>
2022-11-05 15:56:43 -0300

    hotfix modals

--- src/components/modal/ModalCadastro.vue
+++ src/components/modal/ModalCadastro.vue
@@ -1,41 +1,36 @@
 <template>
-    <div id="modalRegister" class="modalOverlay">
-        <div class="modal">
-            <a href="#" class="close">&times;</a>
-            <div class="content">
-                <div class="form-container">
-                    <h3 class="title">Cadastro</h3>
-                    <form action="" class="form-horizontal">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Insira o nome de usuário">
-                        </div>
-
-                        <div class="form-group">
-                            <input type="email" class="form-control" placeholder="Insira o email">
-                        </div>
-
-                        <div class="form-group">
-                            <input type="email" class="form-control" placeholder="Confirme o email">
-                        </div>
-
-                        <div class="form-group">
-                            <input type="tel" class="form-control" placeholder="Insira o número de celular (Opcional)">
-                        </div>
-
-                        <div class="form-group">
-                            <input type="password" class="form-control" placeholder="Insira a senha">
-                        </div>
-
-                        <div class="form-group">
-                            <input type="password" class="form-control" placeholder="Confirme a senha">
-                        </div>
-                        <button class="btn">Cadastrar</button>
-                    </form>
-                    <label class="user-singup">
-                        <a href="modallogin.html">Ja possui uma conta?</a>
-                    </label>
+    <div class="content">
+        <div class="form-container">
+            <h3 class="title">Cadastro</h3>
+            <form action="" class="form-horizontal">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Insira o nome de usuário">
                 </div>
-            </div>
+
+                <div class="form-group">
+                    <input type="email" class="form-control" placeholder="Insira o email">
+                </div>
+
+                <div class="form-group">
+                    <input type="email" class="form-control" placeholder="Confirme o email">
+                </div>
+
+                <div class="form-group">
+                    <input type="tel" class="form-control" placeholder="Insira o número de celular (Opcional)">
+                </div>
+
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Insira a senha">
+                </div>
+
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Confirme a senha">
+                </div>
+                <button class="btn">Cadastrar</button>
+            </form>
+            <label class="user-singup">
+                <a href="#">Ja possui uma conta?</a>
+            </label>
         </div>
     </div>
 </template>
@@ -44,7 +39,7 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'ModalCastro'
+    name: 'ModalRegister'
 })
 
 </script>
--- src/components/modal/ModalLogin.vue
+++ src/components/modal/ModalLogin.vue
@@ -1,30 +1,25 @@
 <template>
-    <div id="modalLogin" class="modalOverlay">
-        <div class="modal">
-            <a href="#" class="close">&times;</a>
-            <div class="content">
-                <div class="form-container">
-                    <h3 class="title">Login</h3>
-                    <form action="" class="form-horizontal">
-                        <div class="form-group">
-                            <input type="email" class="form-control" placeholder="Insira o email">
-                        </div>
-
-                        <div class="form-group">
-                            <input type="password" class="form-control" placeholder="Insira a senha">
-                        </div>
-
-                        <label class="forget">
-                            <a href="modalforget.html">Esqueceu a senha?</a>
-                        </label>
-                        <button class="btn">Login</button>
-                    </form>
-
-                    <label class="user-singup">
-                        <a href="modalcasdastro.html">Ainda não tem uma conta?</a>
-                    </label>
+    <div class="content">
+        <div class="form-container">
+            <h3 class="title">Login</h3>
+            <form action="" class="form-horizontal">
+                <div class="form-group">
+                    <input type="email" class="form-control" placeholder="Insira o email">
                 </div>
-            </div>
+
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Insira a senha">
+                </div>
+
+                <label class="forget">
+                    <a href="modalforget.html">Esqueceu a senha?</a>
+                </label>
+                <button class="btn">Login</button>
+            </form>
+
+            <label class="user-singup">
+                <a href="#">Ainda não tem uma conta?</a>
+            </label>
         </div>
     </div>
 </template>
